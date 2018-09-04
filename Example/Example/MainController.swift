506f65061c348406615002508e327b64ec74938b
Ermat <ealymbaev@gmail.com>
2018-09-04 15:58:47 +0600

    WalletKit Example: add tab bar icons

--- Example/Example/MainController.swift
+++ Example/Example/MainController.swift
@@ -7,15 +7,19 @@ class MainController: UITabBarController {
 
         let balanceNavigation = UINavigationController(rootViewController: BalanceController())
         balanceNavigation.tabBarItem.title = "Balance"
+        balanceNavigation.tabBarItem.image = UIImage(named: "Balance Tab Bar Icon")
 
         let transactionsNavigation = UINavigationController(rootViewController: TransactionsController())
         transactionsNavigation.tabBarItem.title = "Transactions"
+        transactionsNavigation.tabBarItem.image = UIImage(named: "Transactions Tab Bar Icon")
 
         let sendNavigation = UINavigationController(rootViewController: SendController())
         sendNavigation.tabBarItem.title = "Send"
+        sendNavigation.tabBarItem.image = UIImage(named: "Send Tab Bar Icon")
 
         let receiveNavigation = UINavigationController(rootViewController: ReceiveController())
         receiveNavigation.tabBarItem.title = "Receive"
+        receiveNavigation.tabBarItem.image = UIImage(named: "Receive Tab Bar Icon")
 
         viewControllers = [balanceNavigation, transactionsNavigation, sendNavigation, receiveNavigation]
     }
