3a1c7d5c6cc8ea4bfa044aab9a4dfecea8a2b8f4
Anton Stavnichiy <md.belichenko@gmail.com>
2018-09-21 13:44:40 +0600

    #13: Implement BitcoinCash validator.
    - Make real test for validate.
    - Add old EDA validator.
    - Add condition for first 147 block to avoid validate.

--- WalletKit/WalletKit/BlockHeaders/Validators/DAAValidator.swift
+++ WalletKit/WalletKit/BlockHeaders/Validators/DAAValidator.swift
@@ -2,9 +2,8 @@ import Foundation
 import BigInt
 
 class DAAValidator: IBlockValidator {
-//    private let largestHash = BigInt(1) << 256
-//    let medianTimeSpanCount = 11
-//
+    private let largestHash = BigInt(1) << 256
+
     let difficultyEncoder: DifficultyEncoder
     let blockHelper: BlockHelper
 
@@ -13,67 +12,65 @@ class DAAValidator: IBlockValidator {
         self.blockHelper = blockHelper
     }
 
-//    private func work(bits: Int) -> BigInt {
-//        let target = difficultyEncoder.decodeCompact(bits: bits)
-//        let bigInt: BigInt = largestHash / (target + 1)
-//        print("work - \(bigInt.description)")
-//        return bigInt
-//    }
-//
-//    private func limit(timeSpan: Int, targetSpacing: Int) -> Int {
-//        return max(min(288 * targetSpacing, timeSpan), 72 * targetSpacing)
-//    }
-//
-//    private func suitableBlock(for block: Block) throws -> Block {
-//        guard let prevBlock = block.previousBlock, let prevPrevBlock = prevBlock.previousBlock else {
-//            throw BlockValidatorError.noPreviousBlock
-//        }
-//        guard let blockHeader = block.header, let prevBlockHeader = prevBlock.header, let prevPrevBlockHeader = prevPrevBlock.header else {
-//            throw Block.BlockError.noHeader
-//        }
-//        var blockArray = [prevPrevBlock, prevBlock, block]
-//        // Sorting network.
-//        if (blockArray[0].header!.timestamp > blockArray[2].header!.timestamp) {
-//            blockArray.swapAt(0, 2)
-//        }
-//        if (blockArray[0].header!.timestamp > blockArray[1].header!.timestamp) {
-//            blockArray.swapAt(0, 1)
-//        }
-//        if (blockArray[1].header!.timestamp > blockArray[2].header!.timestamp) {
-//            blockArray.swapAt(1, 2)
-//        }
-//        return blockArray[1]
-//    }
-//// 470804401
+    private func work(bits: Int) -> BigInt {
+        let target = difficultyEncoder.decodeCompact(bits: bits)
+        let bigInt: BigInt = largestHash / (target + 1)
+        return bigInt
+    }
+
+    private func limit(timeSpan: Int, targetSpacing: Int) -> Int {
+        return max(min(288 * targetSpacing, timeSpan), 72 * targetSpacing)
+    }
+
+    private func suitableBlock(for block: Block) throws -> Block {
+        guard let prevBlock = block.previousBlock, let prevPrevBlock = prevBlock.previousBlock else {
+            throw BlockValidatorError.noPreviousBlock
+        }
+        guard let blockHeader = block.header, let prevBlockHeader = prevBlock.header, let prevPrevBlockHeader = prevPrevBlock.header else {
+            throw Block.BlockError.noHeader
+        }
+        var blockArray = [prevPrevBlock, prevBlock, block]
+        // Sorting network.
+        if (blockArray[0].header!.timestamp > blockArray[2].header!.timestamp) {
+            blockArray.swapAt(0, 2)
+        }
+        if (blockArray[0].header!.timestamp > blockArray[1].header!.timestamp) {
+            blockArray.swapAt(0, 1)
+        }
+        if (blockArray[1].header!.timestamp > blockArray[2].header!.timestamp) {
+            blockArray.swapAt(1, 2)
+        }
+        return blockArray[1]
+    }
 
     func validate(candidate: Block, block: Block, network: NetworkProtocol) throws {
-//        guard let candidateHeader = candidate.header else {
-//            throw Block.BlockError.noHeader
-//        }
-//        let previousBlock = block //try suitableBlock(for: candidate)
-//        let firstBlock = try suitableBlock(for: blockHelper.previous(for: block, index: 144)!)
-//        let heightInterval = previousBlock.height - firstBlock.height
-//
-//        guard var blocks = blockHelper.previousWindow(for: candidate, count: heightInterval) else {
-//            throw BlockValidatorError.noPreviousBlock
-//        }
-//        print("FIRST : \(blocks[0].header!.bits)")
-//        print("LaST : \(blocks[blocks.count - 1].header!.bits)")
-//
-//        let timeSpan = limit(timeSpan: blocks[blocks.count - 1].header!.timestamp - blocks[0].header!.timestamp, targetSpacing: network.targetSpacing)
-//
-//        var chainWork = BigInt(0)
-//        for i in 1..<heightInterval {
-//            chainWork += work(bits: blocks[i].header!.bits)
-//        }
-//        let projectedWork = chainWork * BigInt(network.targetSpacing) / BigInt(timeSpan)
-//
-//        let target = largestHash / projectedWork - BigInt(1)
-//        print("TARGET: \(difficultyEncoder.encodeCompact(from: target)) -- candidate: \(candidateHeader.bits)")
-//
-//        guard difficultyEncoder.encodeCompact(from: target) == candidateHeader.bits else {
-//            throw BlockValidatorError.notEqualBits
-//        }
+        guard let candidateHeader = candidate.header else {
+            throw Block.BlockError.noHeader
+        }
+        let lastBlock = try suitableBlock(for: block)
+        let firstBlock = try suitableBlock(for: blockHelper.previous(for: block, index: 144)!)
+        let heightInterval = lastBlock.height - firstBlock.height
+
+        guard var blocks = blockHelper.previousWindow(for: lastBlock, count: heightInterval - 1) else {
+            throw BlockValidatorError.noPreviousBlock
+        }
+        blocks.append(lastBlock)
+
+        let timeSpan = limit(timeSpan: lastBlock.header!.timestamp - firstBlock.header!.timestamp, targetSpacing: network.targetSpacing)
+
+        var chainWork = BigInt(0)
+        for i in 0..<blocks.count {
+            chainWork += work(bits: blocks[i].header!.bits)
+        }
+        let projectedWork = chainWork * BigInt(network.targetSpacing) / BigInt(timeSpan)
+
+        let target = largestHash / projectedWork - BigInt(1)
+
+        let bits = difficultyEncoder.encodeCompact(from: target)
+
+        guard bits == candidateHeader.bits else {
+            throw BlockValidatorError.notEqualBits
+        }
     }
 
 }
--- WalletKit/WalletKit/BlockHeaders/Validators/EDAValidator.swift
+++ WalletKit/WalletKit/BlockHeaders/Validators/EDAValidator.swift
@@ -2,8 +2,6 @@ import Foundation
 import BigInt
 
 class EDAValidator: IBlockValidator {
-//    let medianTimeSpanCount = 11
-
     let difficultyEncoder: DifficultyEncoder
     let blockHelper: BlockHelper
 
@@ -13,30 +11,31 @@ class EDAValidator: IBlockValidator {
     }
 
     func validate(candidate: Block, block: Block, network: NetworkProtocol) throws {
-//        guard let candidateHeader = candidate.header, let blockHeader = block.header else {
-//            throw Block.BlockError.noHeader
-//        }
-//        if blockHeader.bits == network.maxTargetBits {
-//            if candidateHeader.bits != network.maxTargetBits {
-//                throw BlockValidatorError.notEqualBits
-//            }
-//            return
-//        }
-//        guard let cursorBlock = blockHelper.previous(for: block, index: 6) else {
-//            throw BlockValidatorError.noPreviousBlock
-//        }
-//        let mpt6blocks = try blockHelper.medianTimePast(block: block) - blockHelper.medianTimePast(block: cursorBlock)
-//        if(mpt6blocks >= 12 * 3600) {
-//            var pow = difficultyEncoder.decodeCompact(bits: blockHeader.bits) >> 2
-//            pow = min(pow, difficultyEncoder.decodeCompact(bits: network.maxTargetBits))
-//            guard difficultyEncoder.encodeCompact(from: pow) == candidateHeader.bits else {
-//                throw BlockValidatorError.notEqualBits
-//            }
-//        } else {
-//            guard blockHeader.bits == candidateHeader.bits else {
-//                throw BlockValidatorError.notEqualBits
-//            }
-//        }
+        guard let candidateHeader = candidate.header, let blockHeader = block.header else {
+            throw Block.BlockError.noHeader
+        }
+        if blockHeader.bits == network.maxTargetBits {
+            if candidateHeader.bits != network.maxTargetBits {
+                throw BlockValidatorError.notEqualBits
+            }
+            return
+        }
+        guard let cursorBlock = blockHelper.previous(for: block, index: 6) else {
+            throw BlockValidatorError.noPreviousBlock
+        }
+        let mpt6blocks = try blockHelper.medianTimePast(block: block) - blockHelper.medianTimePast(block: cursorBlock)
+        if(mpt6blocks >= 12 * 3600) {
+            var pow = difficultyEncoder.decodeCompact(bits: blockHeader.bits) >> 2
+            pow = min(pow, difficultyEncoder.decodeCompact(bits: network.maxTargetBits))
+
+            guard difficultyEncoder.encodeCompact(from: pow) == candidateHeader.bits else {
+                throw BlockValidatorError.notEqualBits
+            }
+        } else {
+            guard blockHeader.bits == candidateHeader.bits else {
+                throw BlockValidatorError.notEqualBits
+            }
+        }
     }
 
 }
