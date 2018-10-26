870674582835327d02d24deee5463aa2997b0489
Anton Stavnichiy <md.belichenko@gmail.com>
2018-10-26 16:49:54 +0600

    Revert dns seeds.

--- HSBitcoinKit/HSBitcoinKit/PeerNetwork/BitcoinRegTest.swift
+++ HSBitcoinKit/HSBitcoinKit/PeerNetwork/BitcoinRegTest.swift
@@ -20,11 +20,10 @@ class BitcoinRegTest: INetwork {
     let sigHash: SigHashType = .bitcoinAll
 
     let dnsSeeds = [
-        // "blocknode01.grouvi.org",
-        // "blocknode02.grouvi.org",
-        // "blocknode03.grouvi.org",
-        // "blocknode04.grouvi.org",
-        "localhost"
+         "blocknode01.grouvi.org",
+         "blocknode02.grouvi.org",
+         "blocknode03.grouvi.org",
+         "blocknode04.grouvi.org",
     ]
 
     let genesisBlock = Block(
