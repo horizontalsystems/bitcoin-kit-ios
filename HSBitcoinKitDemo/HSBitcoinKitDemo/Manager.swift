e75807ae7b962fe37d02a25fd5ebbeab138ab954
Anton Stavnichiy <md.belichenko@gmail.com>
2018-12-03 14:14:30 +0600

    return testNet.

--- HSBitcoinKitDemo/HSBitcoinKitDemo/Manager.swift
+++ HSBitcoinKitDemo/HSBitcoinKitDemo/Manager.swift
@@ -8,7 +8,7 @@ class Manager {
 
     private let keyWords = "mnemonic_words"
 
-    let coin: BitcoinKit.Coin = .bitcoin(network: .mainNet)
+    let coin: BitcoinKit.Coin = .bitcoin(network: .testNet)
 
     var bitcoinKit: BitcoinKit!
 
