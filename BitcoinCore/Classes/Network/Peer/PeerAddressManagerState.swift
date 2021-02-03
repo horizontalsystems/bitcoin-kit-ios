ef28d8f1e2f299313bf952ab3913ed4d914aa82a
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-02-03 18:33:23 +0600

    Unwrap optional baseAddress of a unsafe bytes object in PeerConnection#send(message:)
    - Wrap all PeerAddressManager calls to storage and state in a serial queue

--- BitcoinCore/Classes/Network/Peer/PeerAddressManagerState.swift
+++ BitcoinCore/Classes/Network/Peer/PeerAddressManagerState.swift
@@ -1,18 +1,13 @@
 import Foundation
 
 class PeerAddressManagerState {
-    private let queue = DispatchQueue(label: "io.horizontalsystems.bitcoin-core.peer-address-manager-state", qos: .utility)
     private(set) var usedIps: [String] = []
 
     func add(usedIp: String) {
-        queue.sync {
-            self.usedIps.append(usedIp)
-        }
+        usedIps.append(usedIp)
     }
 
     func remove(usedIp: String) {
-        queue.sync {
-            self.usedIps.removeAll(where: { $0 == usedIp })
-        }
+        usedIps.removeAll(where: { $0 == usedIp })
     }
 }
