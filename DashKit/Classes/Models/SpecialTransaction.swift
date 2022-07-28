0d5bb7c017e138163d0a0b3cb5de1688982e2d7d
ant013 <ant013@mail.ru>
2022-07-28 16:12:20 +0600

    Make key for forced set hash

--- DashKit/Classes/Models/SpecialTransaction.swift
+++ DashKit/Classes/Models/SpecialTransaction.swift
@@ -4,8 +4,8 @@ import BitcoinCore
 class SpecialTransaction: FullTransaction {
     let extraPayload: Data
 
-    init(transaction: FullTransaction, extraPayload: Data) {
+    init(transaction: FullTransaction, extraPayload: Data, forceHashUpdate: Bool = true) {
         self.extraPayload = extraPayload
-        super.init(header: transaction.header, inputs: transaction.inputs, outputs: transaction.outputs)
+        super.init(header: transaction.header, inputs: transaction.inputs, outputs: transaction.outputs, forceHashUpdate: forceHashUpdate)
     }
 }
